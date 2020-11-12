b0b0f4b9dd60d07cea693838eab6540074370043
linhle <lklinh@outlook.com>
2020-11-12 16:36:16 +0700

    add socket

---
+++ socketApi.js
@@ -0,0 +1,14 @@
+const socket_io = require("socket.io");
+const io = socket_io();
+
+let socketApi = {};
+
+socketApi.io = io;
+
+io.on("connection", function (socket) {
+  let numberOfUser = socket.client.conn.server.clientsCount;
+  let mess = `${numberOfUser} are seeing this`;
+  io.emit("receive", mess);
+});
+
+module.exports = socketApi;
